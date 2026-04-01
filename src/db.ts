4809cb1c9f236326966647dd6cc846d2c1f3cba6
Ben Wainaina <bwainaina@curalinc.com>
2026-04-01 05:53:54 +0300

    log it out

--- src/db.ts
+++ src/db.ts
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+console.log("process.env.MONGODB_URI", process.env.MONGODB_URI);
+
 export const client = new MongoClient(process.env.MONGODB_URI!);
 
 await client.connect();
