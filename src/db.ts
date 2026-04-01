cd5f8d9e688273876ff50773a99bca050213a15d
Ben Wainaina <bwainaina@curalinc.com>
2026-04-01 05:56:57 +0300

    remove undefined options

--- src/db.ts
+++ src/db.ts
@@ -1,10 +1,22 @@
-import { MongoClient } from "mongodb";
+import { MongoClient, MongoClientOptions } from "mongodb";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 console.log("process.env.MONGODB_URI", process.env.MONGODB_URI);
 
-export const client = new MongoClient(process.env.MONGODB_URI!);
+const options: MongoClientOptions = {
+  maxPoolSize: 10,
+};
+
+Object.keys(options).forEach(
+  (key) =>
+    options[key as keyof MongoClientOptions] === undefined &&
+    delete options[key as keyof MongoClientOptions],
+);
+
+console.log("options", options);
+
+export const client = new MongoClient(process.env.MONGODB_URI!, options);
 
 await client.connect();
