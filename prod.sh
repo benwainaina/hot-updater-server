fc0f8d60c0b8df850282bc21fd1dd3802a439c87
Ben Wainaina <bwainaina@curalinc.com>
2026-04-04 19:59:39 +0300

    add run script

--- prod.sh
+++ prod.sh
@@ -2,5 +2,5 @@ serviceName="hot-updater-server"
 pm2 stop $serviceName
 pm2 delete $serviceName
 
-pm2 start npm --name $serviceName --run start
+pm2 start npm --name $serviceName -- run start
 echo "Server started"
